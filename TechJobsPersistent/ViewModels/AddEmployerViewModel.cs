05bce43dcff30b768c0e3fc9203a1e7dd0b8ab41
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-10-20 20:58:02 -0500

    working on part 2

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+
+        public AddEmployerViewModel() { }
+    }
+}
